e2cb5dffb2e966b15b9c2a8c019c9f7058fe54eb
srugh <steve.rugh@hotmail.com>
2025-12-12 11:52:59 -0800

    more aoc solutions

---
+++ 2017/day-16.rb
@@ -0,0 +1,95 @@
+def parse_input(path)
+  arr = []
+  File.read(path).split(",").each do |move|
+    m = move[0]
+    if m == "s"
+      n = move[1..-1]
+      arr.push([m, n.to_i, 0])
+    elsif m == "x"
+      a, b = move[1..-1].split("/")
+      arr.push([m,a.to_i,b.to_i])
+    elsif m == "p"
+      a, b = move[1..-1].split("/")
+      arr.push([m,a,b])
+    end
+  end
+  arr
+end
+
+def solve_part_1(moves)
+  programs = "abcdefghijklmnop"
+
+  moves.each do |move|
+    m, a, b = move
+    case m
+    when "s"
+      programs = programs[-a..-1] + programs[0...-a]
+    when "x"
+      temp = programs[a]
+      programs[a] = programs[b]
+      programs[b] = temp
+    when "p"
+      a_i = programs.index(a)
+      b_i = programs.index(b)
+      temp = programs[a_i]
+      programs[a_i] = programs[b_i]
+      programs[b_i] = temp
+    end
+
+  end
+  programs
+end
+
+def dance_once(programs, moves)
+  moves.each do |move|
+    m, a, b = move
+    case m
+    when "s"
+      programs = programs[-a..-1] + programs[0...-a]
+    when "x"
+      programs = programs.dup
+      programs[a], programs[b] = programs[b], programs[a]
+    when "p"
+      programs = programs.dup
+      a_i = programs.index(a)
+      b_i = programs.index(b)
+      programs[a_i], programs[b_i] = programs[b_i], programs[a_i]
+    end
+  end
+  programs
+end
+
+def solve_part_2(moves)
+  start = "abcdefghijklmnop"
+  seen = {}           # state -> index
+  states = []         # index -> state
+
+  current = start
+  i = 0
+
+  loop do
+    if seen.key?(current)
+      cycle_start = seen[current]
+      cycle_len   = i - cycle_start
+
+      # We want the state after 1_000_000_000 dances
+      target = 1_000_000_000
+
+      # We only care about the part inside the cycle
+      offset_in_cycle = (target - cycle_start) % cycle_len
+      return states[cycle_start + offset_in_cycle]
+    end
+
+    seen[current] = i
+    states << current
+
+    current = dance_once(current, moves)
+    i += 1
+  end
+end
+
+
+path = "inputs/day-16.txt"
+moves = parse_input(path)
+puts "part 1: #{solve_part_1(moves)}"
+puts "part 2: #{solve_part_2(moves)}"
---
+++ 2025/day-12.rb
@@ -0,0 +1,41 @@
+def parse_input(path)
+  shapes = []
+  trees = []
+  parts = File.read(path).split(/\n\n/)
+
+  parts[0...parts.size-1].each do |part|
+    lines = part.split(/\n/)
+    shape = []
+    lines[1..-1].each_with_index do |line, r|
+      line.chars.each_with_index do |ch, c|
+        shape << [r, c] if ch == "#"
+      end
+    end
+    shapes << shape
+  end
+
+  parts.last.split(/\n/).each do |line|
+    l, r = line.split(":")
+    trees << [l.split("x").map(&:to_i), r.split.map(&:to_i)]
+  end
+
+  [shapes, trees]
+end
+
+def solve_part_1(shapes, trees)
+  shape_areas  = shapes.map(&:size)
+  count = 0
+  trees.each do |(width, height), inventory|
+    t_area = width * height
+    p_area = 0
+    inventory.each_with_index do |inv, i|
+      p_area += inv * shape_areas[i]
+    end
+    count += 1 if t_area > p_area
+  end
+  count
+end
+
+path = "Inputs/day-12.txt"
+shapes, trees = parse_input(path)
+puts "part 1: #{solve_part_1(shapes, trees)}"
